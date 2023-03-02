8f1de36da54182ddeebdee9d61574cf4b2d5cce2
Etienne H <contact-github@halletienne.fr>
2023-03-02 17:06:56 +0100

    first draft for waarp request

---
+++ app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template
+
+DEVELOPMENT_ENV = True
+app = Flask(__name__)
+
+
+app_data = {
+    "name": "Manutan Waarp Flow request system",
+    "description": "Basic site to manage waarp flows",
+    "author": "Manutan BT",
+    "html_title": "Template for a Flask Web App",
+    "project_name": "Waarp Requests",
+    "keywords": "flask, webapp, template, basic, waarp",
+}
+
+@app.route("/")
+def hello_world():
+        return "<p>Hello, World!</p>"
+    
+
+@app.route("/flows")
+def list_flows():
+    return render_template("flow_list.html",app_data=app_data)
+
+
+if __name__ == "__main__":
+    app.run(debug=DEVELOPMENT_ENV, host="0.0.0.0")
---
+++ models.py
@@ -0,0 +1,86 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from .database import Base
+
+
+class Partner(Base):
+    __tablename__ = 'partners'
+    id = Column(Integer, primary_key=True)
+    site = Column(Integer)
+    type = Column(String(10))
+    isClient = Column(Boolean())
+    isServer = Column(Boolean())
+    description = Column(String(20))
+    hostid = Column(String(30))
+    hostidssl = Column(String(30))
+
+
+    def __init__(self, id, site=1, type='r66', isClient=True, isServer=True, description='', hostid='', hostidssl=''):
+        self.id = id
+        self.site = site
+        self.type = type
+        self.isClient = isClient
+        self.isServer = isServer
+        self.description = description
+        self.hostid = hostid
+        self.hostidssl = hostidssl
+
+    def __repr__(self):
+        return f'<Partner {self.hostid!r}>'
+
+
+class Site(Base):
+    __tablename__ = 'sites'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(10))
+    description = Column(String(20))
+
+
+    def __init__(self, id, name='', description='', ):
+        self.id = id
+        self.name = name
+        self.description = description
+
+    def __repr__(self):
+        return f'<Site {self.name!r}>'
+
+
+class Flow(Base):
+    __tablename__ = 'flows'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+    active = Column(Boolean())
+    template = Column(Integer)
+    origin = Column(Integer)
+    originDir = Column(String(40))
+    # Destination ? 
+    description = Column(String(20))
+    filewatcher = Column(Boolean())
+
+
+
+    def __init__(self, id, name='', active=True, template=3, origin=0, originDir='', description='', filewatcher=True):
+        self.id = id
+        self.name = name
+        self.active = active
+        self.template = template
+        self.origin = origin
+        self.originDir = originDir
+        self.description = description
+        self.filewatcher = filewatcher
+
+    def __repr__(self):
+        return f'<Flow {self.name!r}>'
+
+
+class FlowTemplate(Base):
+    __tablename__ = 'flowtemplates'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+
+    def __init__(self, id, name='' ):
+        self.id = id
+        self.name = name
+
+    def __repr__(self):
+        return f'<FlowTemplate {self.name!r}>'
+
