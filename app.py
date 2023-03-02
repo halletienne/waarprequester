8af669ec35d6beaa9b62019e4fdc5826198ad408
Etienne H <contact-github@halletienne.fr>
2023-03-03 00:18:49 +0100

    Starting adding requests

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
 from database import db_session
-from models import Flow, Partner, Site, FlowTemplate
+from markupsafe import escape
+from models import Flow, Partner, Site, FlowTemplate, PartnerRequest, FlowRequest
 DEVELOPMENT_ENV = True
 app = Flask(__name__)
 
@@ -22,13 +23,93 @@ def hello_world():
 
 @app.route("/flows")
 def list_flows():
-    return render_template("flow_list.html",app_data=app_data)
+    flows = Flow.query.filter(Flow.waarp_id < 50 )
+    partners = Partner.query.all()
+    partner_linked = [
+        {
+            "waarp_id": partner.waarp_id,
+            "hostid": partner.hostid
+        } for partner in partners ]
+
+    flow_results = [
+              {
+                  "waarp_id": flow.waarp_id,
+                  "name": flow.name,
+                  "description": flow.description,
+                  "active": flow.active,
+                  "origin": flow.origin
+                  } for flow in flows]
+    return render_template("flow_list.html",app_data=app_data, flows=flow_results, partners=partner_linked)
 
 @app.route("/partners")
 def list_partners():
-    partners = Partner.query.filter(Partner.id < 50 )
-    return render_template("partners_list.html",app_data=app_data, partners=partners)
+    partners = Partner.query.filter(Partner.waarp_id < 50 )
+    partner_results = [
+              {
+                  "waarp_id": partner.waarp_id,
+                  "hostid": partner.hostid,
+                  "hostidssl": partner.hostidssl,
+                  "type": partner.type,
+                  "description": partner.description,
+                  "isClient": partner.isClient,
+                  "isServer": partner.isServer
+                  } for partner in partners]
+    return render_template("partner_list.html",app_data=app_data, partners=partner_results)
 
+
+@app.route("/requests")
+def show_request():
+    flow_requests = FlowRequest.query.all()
+
+    flow_request_results = [
+              {
+                  "request_id": flow.id,
+                  "name": flow.name,
+                  "active": flow.active,
+                  "template": flow.template,
+                  "origin": flow.origin,
+                  "originDir": flow.originDir,
+                  "description": flow.description,
+                  "filewatcher": flow.filewatcher
+                  } for flow in flow_requests]
+
+    partner_requests = PartnerRequest.query.all()
+
+    partner_request_results = [
+              {
+                  "request_id": partner.id,
+                  "site": partner.site,
+                  "type": partner.type,
+                  "isClient": partner.isClient,
+                  "isServer": partner.isServer,
+                  "description": partner.description,
+                  "hostid": partner.hostid,
+                  "hostidssl": partner.hostidssl
+                  } for partner in partner_requests]
+
+    return render_template("requests_list.html", app_data=app_data, partner_requests=partner_request_results, flow_requests=flow_request_results)
+
+@app.route("/partner/<waarp_id>")
+def show_partner(waarp_id):
+    safe_id = escape(waarp_id)
+    partner_query = Partner.query.filter(Partner.waarp_id == safe_id).first()
+    partner = {
+       "hostid": partner_query.hostid,
+       "site": partner_query.site,
+       "type": partner_query.type
+       }
+    flows = Flow.query.filter(Flow.origin == safe_id )
+    flow_results = [
+              {
+                  "waarp_id": flow.waarp_id,
+                  "name": flow.name,
+                  "description": flow.description,
+                  "active": flow.active,
+                  "origin": flow.origin
+                  } for flow in flows]
+    return render_template('partner_detail.html', app_data=app_data, partner=partner, flows=flow_results)
+
+    
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
