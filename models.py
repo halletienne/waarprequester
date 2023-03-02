08871f6fee71d76b413b89720fd2b3670fefe22b
Etienne Hallet <etienne.hallet@manutan.fr>
2023-03-02 17:45:04 +0100

    Ajout du model de DB pour les requests

--- models.py
+++ models.py
@@ -84,3 +84,57 @@ class FlowTemplate(Base):
     def __repr__(self):
         return f'<FlowTemplate {self.name!r}>'
 
+
+class PartnerRequest(Base):
+    __tablename__ = 'partner_requests'
+    id = Column(Integer, primary_key=True)
+    site = Column(Integer)
+    type = Column(String(10))
+    isClient = Column(Boolean())
+    isServer = Column(Boolean())
+    description = Column(String(20))
+    hostid = Column(String(30))
+    hostidssl = Column(String(30))
+
+
+    def __init__(self, id, site=1, type='r66', isClient=True, isServer=True, description='', hostid='', hostidssl=''):
+        self.id = id
+        self.site = site
+        self.type = type
+        self.isClient = isClient
+        self.isServer = isServer
+        self.description = description
+        self.hostid = hostid
+        self.hostidssl = hostidssl
+
+    def __repr__(self):
+        return f'<Partner {self.hostid!r}>'
+
+
+
+class FlowRequest(Base):
+    __tablename__ = 'flow_requests'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+    active = Column(Boolean())
+    template = Column(Integer)
+    origin = Column(Integer)
+    originDir = Column(String(40))
+    # Destination ? 
+    description = Column(String(20))
+    filewatcher = Column(Boolean())
+
+
+
+    def __init__(self, id, name='', active=True, template=3, origin=0, originDir='', description='', filewatcher=True):
+        self.id = id
+        self.name = name
+        self.active = active
+        self.template = template
+        self.origin = origin
+        self.originDir = originDir
+        self.description = description
+        self.filewatcher = filewatcher
+
+    def __repr__(self):
+        return f'<FlowRequest {self.name!r}>'        
